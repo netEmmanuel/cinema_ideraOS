8551407471e86cee5d0b3450a85d8ea9463e082c
damilare <d.dammylarry@gmail.com>
2019-07-24 11:12:53 +0100

    feat: add migration for cinema and movies table

---
+++ database/migrations/2019_07_24_095917_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('movie_title');
+            $table->string("movie_banner");
+            $table->date('start_time');
+            $table->date('end_time');
+            $table->integer('cinema_id')->unsigned()->index('movies_cinema_id_foreign');
+
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
